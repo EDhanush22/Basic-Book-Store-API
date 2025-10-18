6d808426159d743395b5b9fd94a6c590534ef274
E.Dhanush <dhanushenugu22052006@gmail.com>
2025-10-18 20:45:35 +0530

    Initial commit

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectToDB = async() => {
     try {
-        await mongoose.connect('mongodb+srv://dhanush22:dhanush22@bookdevelopementapi.flw5cbi.mongodb.net/?retryWrites=true&w=majority&appName=BookDevelopementAPI');
+        await mongoose.connect('mongodb+srv://{Your mongoDB user ID}:{Your MongoDB password}@bookdevelopementapi.flw5cbi.mongodb.net/?retryWrites=true&w=majority&appName=BookDevelopementAPI');
         console.log('mongodb is connected successfully')
     } catch(error) {
         console.error('Mongodb connection failed', error)
